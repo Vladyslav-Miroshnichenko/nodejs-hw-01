0813bc811bef6b1cee176321d8da1954a428e870
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-11-30 17:44:27 +0100

    fix

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,5 +2,5 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
-  await fs.appendFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
 };
