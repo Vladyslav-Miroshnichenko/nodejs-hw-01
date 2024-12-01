5ade028741a3cd3fbe02a8db8ad5a4e6c884d522
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-12-01 12:10:46 +0100

    bug-fix

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,6 +1,6 @@
-import { readContacts } from '../utils/readContacts';
-import { createFakeContact } from '../utils/createFakeContact';
-import { writeContacts } from '../utils/writeContacts';
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 export const addOneContact = async () => {
   const contactList = await readContacts();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,5 @@
-import { readContacts } from '../utils/readContacts';
+import { readContacts } from '../utils/readContacts.js';
 
-export const getAllContacts = async () => readContacts();
+export const getAllContacts = async () => await readContacts();
 
 console.log(await getAllContacts());
